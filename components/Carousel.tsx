a9b5f57a73fed281083d6cbccd3fdeaaec5b6dff
Kevlar277 <rkabrasada@up.edu.ph>
2024-05-21 23:35:24 +0800

    Update Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -15,27 +15,29 @@ const Carousel = () => {
   };
 
   return (
-    <Slider {...settings}>
-      <div>
-        <img src="/img/upmin.jpg" alt="Image 1" />
-      </div>
-      <div>
-        <img src="/img/1.png" alt="Image 2" />
-      </div>
-      <div>
-        <img src="/img/2.png" alt="Image 3" />
-      </div>
-      <div>
-        <img src="/img/3.png" alt="Image 4" />
-      </div>
-      <div>
-        <img src="/img/4.png" alt="Image 5" />
-      </div>
-      <div>
-        <img src="/img/5.png" alt="Image 6" />
-      </div>
-      {/* Add more images as needed */}
-    </Slider>
+    <div className='mx-auto max-w-xl sm:max-w-xl md:max-w-2xl lg:max-w-2xl xl:max-w-2xl'>
+      <Slider {...settings}>
+        <div>
+          <img src="/img/upmin.jpg" alt="Image 1" />
+        </div>
+        <div>
+          <img src="/img/1.png" alt="Image 2" />
+        </div>
+        <div>
+          <img src="/img/2.png" alt="Image 3" />
+        </div>
+        <div>
+          <img src="/img/3.png" alt="Image 4" />
+        </div>
+        <div>
+          <img src="/img/4.png" alt="Image 5" />
+        </div>
+        <div>
+          <img src="/img/5.png" alt="Image 6" />
+        </div>
+        {/* Add more images as needed */}
+      </Slider>
+    </div>
   );
 };
 
