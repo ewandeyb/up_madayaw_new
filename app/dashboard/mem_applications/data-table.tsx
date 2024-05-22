bc071d6498ce1279a60e5db26e81cd97e14964a9
Aaron Siapuatco <aaronsiapuatco@gmail.com>
2024-05-22 20:14:47 +0800

    fixed filter for email

--- app/dashboard/mem_applications/data-table.tsx
+++ app/dashboard/mem_applications/data-table.tsx
@@ -1,25 +1,25 @@
-"use client"
-import * as React from "react"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+"use client";
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
-ColumnDef,
-ColumnFiltersState,
-SortingState,
-VisibilityState,
-flexRender,
-getCoreRowModel,
-getFilteredRowModel,
-getPaginationRowModel,
-getSortedRowModel,
-useReactTable,
-} from "@tanstack/react-table"
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 
 import {
   Table,
@@ -28,24 +28,25 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "../../../components/ui/table"
-import CreateMember from "../members/components/create/CreateMember"
+} from "../../../components/ui/table";
+import CreateMember from "../members/components/create/CreateMember";
 
 interface DataTableProps<TData, TValue> {
-columns: ColumnDef<TData, TValue>[]
-data: TData[]
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
 }
 
-export function DataTable<TData,TValue>({
+export function DataTable<TData, TValue>({
   columns,
   data,
-}: DataTableProps<TData,TValue>) {
-  const [sorting, setSorting] = React.useState<SortingState>([])
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
-  )
-  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
-  const [rowSelection, setRowSelection] = React.useState({})
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+
   const table = useReactTable({
     data,
     columns,
@@ -56,14 +57,12 @@ export function DataTable<TData,TValue>({
     onColumnFiltersChange: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
     state: {
       sorting,
       columnFilters,
       columnVisibility,
-      rowSelection,
     },
-  })
+  });
 
   return (
     <div>
@@ -75,97 +74,93 @@ export function DataTable<TData,TValue>({
             onChange={(event) =>
               table.getColumn("Email")?.setFilterValue(event.target.value)
             }
-            className="max-w-sm"
+            className="max-w-sm "
           />
           <div className="ml-2">
             <CreateMember />
           </div>
           <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              Columns
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter(
-                (column) => column.getCanHide()
-              )
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                )
-              })}
-          </DropdownMenuContent>
-        </DropdownMenu>
-          
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="ml-auto">
+                Columns
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  );
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key = {headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key = {header.id} className="bg-slate-100">
-                    {header.isPlaceholder
-                      ? null
-                      :flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow 
-                key = {row.id}
-                data-state = {row.getIsSelected() && "selected"}
-                className="bg-white"
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key = {cell.id}>
-                    {flexRender(cell.column.columnDef.cell,cell.getContext())}
-                  </TableCell>
-                ))}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id} className="bg-slate-100">
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan = {columns.length} className = "h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-    <div>
-      
-    </div>
-
-    <div className="flex-1 text-sm text-muted-foreground p-5">
-        {table.getFilteredSelectedRowModel().rows.length} of{" "}
-        {table.getFilteredRowModel().rows.length} row(s) selected.
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  className="bg-white"
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
       </div>
-    <div className="flex items-center justify-end space-x-2">
-    <Button
+      <div></div>
+      <div className="flex items-center justify-end space-x-2 py-5">
+        <Button
           variant="outline"
           size="sm"
           onClick={() => table.setPageIndex(0)}
@@ -198,5 +193,5 @@ export function DataTable<TData,TValue>({
         </Button>
       </div>
     </div>
-  )
+  );
 }
--- app/dashboard/members/components/edit/EditMember.tsx
+++ app/dashboard/members/components/edit/EditMember.tsx
@@ -5,18 +5,27 @@ import { Pencil1Icon } from "@radix-ui/react-icons";
 import EditForm from "./EditorForm";
 import { IPermission } from "@/lib/types";
 
-export default function EditMember({isAdmin,permission}:{isAdmin:boolean,permission:IPermission}) {
-	return (
-		<DialogForm
-		id="update-trigger"
-		title="Edit Member"
-			Trigger={
-				<Button variant="dropdown" className=" font-normal max-h-5 justify-start">
-					<Pencil1Icon />
-					Edit
-				</Button>
-			}
-			form={<EditForm isAdmin={isAdmin} permission={permission}/>}
-		/>
-	);
+export default function EditMember({
+  isAdmin,
+  permission,
+}: {
+  isAdmin: boolean;
+  permission: IPermission;
+}) {
+  return (
+    <DialogForm
+      id="update-trigger"
+      title="Edit Member"
+      Trigger={
+        <Button
+          variant="dropdown"
+          className=" font-normal max-h-5 justify-start"
+        >
+          <Pencil1Icon />
+          Edit
+        </Button>
+      }
+      form={<EditForm isAdmin={isAdmin} permission={permission} />}
+    />
+  );
 }
