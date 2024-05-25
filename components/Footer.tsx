0ad6032a4b687299e9923b21557e9966901b7f5a
Kevlar277 <rkabrasada@up.edu.ph>
2024-05-25 09:11:41 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,17 +3,17 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <>
-    <footer className="p-6 md:py-12 bg-slate-200 dark:bg-gray-800 text-center " >
-        <div className="flex flex-col items-center gap-2 text-sm">
+      <footer className="p-6 md:py-12 bg-gradient-to-r from-[#565656] to-[#303030] dark:bg-gradient-to-r dark:from-[#FFFFFF] dark:to-[#8E8E8E] text-center">
+        <div className="flex flex-col items-center gap-2 text-sm text-white">
           <div className="flex gap-4">
-              <Link className="text-gray-500 hover:underline underline-offset-4 dark:text-gray-400" href="#">
-                Contact us: 09325420044
-              </Link>
+            <Link className="text-white hover:underline underline-offset-4 dark:text-black" href="#">
+              Contact us: 09325420044
+            </Link>
           </div>
-          <a href="#" className="float-right text-gray-500 dark:text-gray-400">Kalimudan Student Center, University of the Philippines Mindanao, Mintal, Tugbok District, Davao City</a>
-          <p className="text-wrap text-gray-500 dark:text-gray-400">© 2010-2024 UP Madayaw. All rights reserved.</p>
+          <a href="#" className="float-right text-white dark:text-black">Kalimudan Student Center, University of the Philippines Mindanao, Mintal, Tugbok District, Davao City</a>
+          <p className="text-wrap text-white dark:text-black">© 2010-2024 UP Madayaw. All rights reserved.</p>
         </div>
-    </footer>
+      </footer>
     </>
   );
 }
