f71b61ee9ffd542a02bfff611444009091b96596
Kevlar277 <rkabrasada@up.edu.ph>
2024-05-25 09:13:27 +0800

    Update page.tsx

--- app/updates/page.tsx
+++ app/updates/page.tsx
@@ -4,11 +4,11 @@ import Updates from '@/components/Updates';
 export default function About() {
   return (
     <>
-      <section className="w-full bg-gray-100 py-10 dark:bg-gray-800 min-h-[400px]">
+      <section className="w-full py-10 bg-gradient-to-r from-[#FFFFFF] to-[#8E8E8E] dark:bg-gradient-to-r dark:from-[#B2B2B2] dark:to-[#202020] min-h-[400px]">
         <div className="container px-4 md:px-6">
           <Updates />
         </div>
-      </section>      
+      </section>
     </>
   );
 }
