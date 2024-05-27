aa18f28b95e44024c7d54c4733d0c0205e9c4663
Kevlar277 <rkabrasada@up.edu.ph>
2024-05-27 20:45:13 +0800

    Update index.ts

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -4,7 +4,24 @@ export type IPermission = {
   Role: "user" | "admin";
   Status: "active" | "resigned";
   MemberData: {
+    CivilStatus: string | undefined;
+    BirthDate: string | undefined;
     MembershipID: string;
+    MembershipNo: string;
+    FirstName: string;
+    LastName: string;
+    Email: string;
+    MemberType: string;
+  };
+};
+
+export type IProfile = {
+  MembershipID: string;
+  created_at: string;
+  MemberData: {
+    CivilStatus: string | undefined;
+    BirthDate: string | undefined;
+    MembershipNo: string;
     FirstName: string;
     LastName: string;
     Email: string;
