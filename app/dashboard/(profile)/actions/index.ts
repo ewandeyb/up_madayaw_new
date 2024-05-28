e7c86a648b43772daac0cfeb5d089936060f9d43
Kevlar277 <rkabrasada@up.edu.ph>
2024-05-28 15:09:26 +0800

    Update index.ts

--- app/dashboard/(profile)/actions/index.ts
+++ app/dashboard/(profile)/actions/index.ts
@@ -5,7 +5,6 @@ import { revalidatePath, unstable_noStore } from "next/cache";
 import { createClient } from '@supabase/supabase-js';
 import { IMemberData } from "@/lib/types";
 import { getUser } from "@/utils/supabase/auth"; // Assuming you have an auth utility to get the logged-in user
-import { useRouter } from 'next/router';
 
 const supabase = createClient('https://hudheaqnruaponeloslj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1ZGhlYXFucnVhcG9uZWxvc2xqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4MDM0MTQsImV4cCI6MjAzMDM3OTQxNH0.k1V6fGk48AiZWeoeAqkqC0MZEjcUxyjOv2eqxwhd48A');
 
